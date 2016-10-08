195238e00db269cb3ec1d4e1c6f724264af05d81
Sujay Raj <sujayraaj@gmail.com>
2016-10-08 16:20:18 +0530

    Moved JS to separate files.
    Now we can just 'require' Jquery and use it.
    
    Author's note: This thing is terrific :D
    
    Signed-off-by: Sujay Raj <sujayraaj@gmail.com>

---
+++ scripts.js
@@ -0,0 +1,9 @@
+$ = require("./bower_components/jquery/dist/jquery.js");
+/*	$('#tab1').on('load',() => {
+		console.log($(this).title);
+	});
+*/
+
+console.log($);
+
+$(document).on('click', ()=> { console.log('clicked')});
