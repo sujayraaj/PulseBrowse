53fce4b9febb829b363fc2160d9b93b2f9f3f99d
Sujay Raj <sujayraaj@gmail.com>
2016-10-08 18:08:39 +0530

    Folder Structure changed. A demo of how app will look.
    I have planned to use the Jquery-ui dialog to handle the tabs.
    From the way things are now it looks good.
    
    Also an assets folder has been created that will hold on to the
    scripts and styles.
    
    Signed-off-by: Sujay Raj <sujayraaj@gmail.com>

--- scripts.js
+++
@@ -1,9 +0,0 @@
-$ = require("./bower_components/jquery/dist/jquery.js");
-/*	$('#tab1').on('load',() => {
-		console.log($(this).title);
-	});
-*/
-
-console.log($);
-
-$(document).on('click', ()=> { console.log('clicked')});
